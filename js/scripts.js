718e8c70b907dad9da1bb4e9c33a2655f06f064d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-06-22 01:49:55 +0300

    Modifies scripts.js file adds validation for contact form inputs

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@
 ***************/
 const iconsDescriptionWrapper="delani-what-we-do";
 const portfolios= ['work1','work2','work3','work4','work5','work6','work7','work8'];
+const alphaNumericPattern = /[^a-z\d]/i;
+const emailPattern = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 const iconDescriptions =[
 	{'name':'design','icon':'design_icon.png',
@@ -15,7 +17,51 @@ const iconDescriptions =[
 	'desc':'<p>Planning and development is iterative. Because we are constantly coding and testing, the products we build are always ready to go live.</p><p> This iterative process allows for changes as business requirements evolve.</p>'}
 ];
 
-var capitalizeEachWord = (sentence) => {
+let validateContactForm = (c_name,c_email,c_message) =>{
+	var isFormValid;
+	if(validateInput(emailPattern,c_email)){
+		if(!validateInput(alphaNumericPattern,c_name)){
+			isFormValid=1002;//Empty name provided
+		}else{
+			if(!validateInput(alphaNumericPattern,c_message)){
+				isFormValid=1003;//INvalid Message
+			}else{
+				isFormValid = 1000;
+			}
+		}
+	}else{
+		isFormValid = 1001;//Invalid Email Address
+	}
+	return isFormValid;
+}
+
+let validateInput = (pattern,input) => {
+	return pattern.test(input);
+}
+
+let clearContactFormErrors = (inputType) =>{
+	 switch(inputType){
+	 	case 'email':
+		document.getElementById("contact_email_error").innerHTML="";
+	 	break;
+
+	 	case 'name':
+		document.getElementById("contact_name_error").innerHTML="";
+	 	break;
+
+	 	case 'message':
+		document.getElementById("contact_message_error").innerHTML="";
+	 	break;
+
+	 	case 'all':
+		document.getElementById("contact_name_error").innerHTML="";
+		document.getElementById("contact_email_error").innerHTML="";
+		document.getElementById("contact_message_error").innerHTML="";
+	 	break;
+	 }
+}
+
+let capitalizeEachWord = (sentence) => {
   return sentence.toLowerCase().split(' ').map((word) => word.charAt(0).toUpperCase()+word.slice(1)).join(' ');
 };
 
@@ -65,7 +111,34 @@ $(document).ready( () => {
     },() => {
 	    	$( "."+portfolio+"-project").hide();
     });
-
 	});
+
+	$("#delani_contact_form").submit((event) => {
+     event.preventDefault();
+     var c_name=$('#contact_name').val();
+     var c_email=$('#contact_email').val();
+     var c_message=$('#contact_message').val();
+     clearContactFormErrors("all");
+     switch(validateContactForm(c_name,c_email,c_message)){
+     		case 1000:
+     		alert("Yeah");
+     		break;
+
+     		case 1001:
+     		$('#contact_email_error').show();
+				document.getElementById("contact_email_error").innerHTML="Kindly provide a valid email address.";
+     		break;
+
+     		case 1002:
+     		$('#contact_name_error').show();
+				document.getElementById("contact_name_error").innerHTML="Kindly provide your name";
+     		break;
+
+     		case 1003:
+     		$('#contact_message_error').show();
+				document.getElementById("contact_message_error").innerHTML="Kindly provide contact message.";
+     		break;
+     }
+  });
 
 });
